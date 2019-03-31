2970fadd7fe15789922832d7552c4f942511912a
Roshan Gulgulia <roshan.gulgulia.rg@gmail.com>
2019-04-01 03:25:49 +0530

    Add files via upload

---
+++ Spring-web-mvc/src/com/springdemo/controller.java
@@ -0,0 +1,15 @@
+package com.springdemo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class controller {
+	
+	@RequestMapping("/")
+	public String getfile()
+	{
+		return "roshan";
+	}
+
+}
